83f022b0c81ed126b88ca68ecc5a1d587bfce381
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-02-16 15:43:51 +0100

    initial setup

---
+++ src/lib/navigation.ts
@@ -0,0 +1,21 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+    ],
+  },
+  {
+    title: 'Core concepts',
+    links: [
+      { title: 'Configuration', href: '/docs/configuration' },
+      { title: 'Dependency Injection', href: '/docs/dependency-injection' },
+    ],
+  },
+  {
+    title: 'Packages',
+    links: [
+      { title: 'Example Package', href: '/docs/example-package' },
+    ],
+  },
+]

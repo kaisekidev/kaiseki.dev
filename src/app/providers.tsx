76ae351063db372f2b76f780963a2a7880e78c3a
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-02-18 14:55:44 +0100

    setup plausible tracking

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import PlausibleProvider from 'next-plausible'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
+  const isProduction = process.env.NODE_ENV === 'production';
+  const element = (
     <ThemeProvider attribute="class" disableTransitionOnChange>
       {children}
     </ThemeProvider>
   );
+  return isProduction ? <PlausibleProvider domain="kaiseki.dev">{element}</PlausibleProvider> : element;
 }

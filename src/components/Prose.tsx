3ef742fe0156518f6449b0ef1e317ef128aef8c3
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-02-19 13:12:53 +0100

    update prose link styles

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -13,7 +13,7 @@ export function Prose<T extends React.ElementType = 'div'>({
     <Component
       className={clsx(
         className,
-        'prose prose-slate max-w-none dark:prose-invert dark:text-neutral-400',
+        'prose prose-slate max-w-none dark:prose-invert dark:text-paper',
         // headings
         'prose-headings:scroll-mt-28 prose-headings:font-display prose-headings:font-normal lg:prose-headings:scroll-mt-[8.5rem]',
         // H2
@@ -25,9 +25,7 @@ export function Prose<T extends React.ElementType = 'div'>({
         // lead
         'prose-lead:text-neutral-500 dark:prose-lead:text-neutral-400',
         // links
-        'prose-a:font-semibold dark:prose-a:text-red-400',
-        // link underline
-        'prose-a:no-underline prose-a:shadow-[inset_0_-2px_0_0_var(--tw-prose-background,#fff),inset_0_calc(-1*(var(--tw-prose-underline-size,4px)+2px))_0_0_var(--tw-prose-underline,theme(colors.red.300))] hover:prose-a:[--tw-prose-underline-size:6px] dark:[--tw-prose-background:theme(colors.slate.900)] dark:prose-a:shadow-[inset_0_calc(-1*var(--tw-prose-underline-size,2px))_0_0_var(--tw-prose-underline,theme(colors.red.800))] dark:hover:prose-a:[--tw-prose-underline-size:6px]',
+        'prose-a:text-blue-700 dark:prose-a:text-blue-400 hover:prose-a:text-blue-800 dark:hover:prose-a:text-blue-500 prose-a:underline prose-a:shadow-none ',
         // pre
         'prose-pre:rounded-md prose-pre:bg-neutral-900 prose-pre:shadow-lg dark:prose-pre:bg-neutral-800/60 dark:prose-pre:shadow-none dark:prose-pre:ring-1 dark:prose-pre:ring-neutral-300/10',
         // hr
